157ae9f7f8835ff760581bfb86c411ffa6cea08c
kobuki <joris.sittler@minesdedouai.fr>
2015-02-27 15:26:48 +0100

    corrections et simplification loop

--- ArduinoMega/ArduinoMega.ino
+++ ArduinoMega/ArduinoMega.ino
@@ -21,36 +21,48 @@ void servo_f(const std_msgs::UInt16& cmd_msg)
 {
    if(cmd_msg.data==0){
         servo1.write(25);
-        digitalWrite(13,HIGH);
-        delay(200);
-        digitalWrite(13,LOW);
    }
    if(cmd_msg.data==1){
         servo1.write(170);
-        digitalWrite(13,HIGH);
-        delay(500);
-        digitalWrite(13,LOW);
    }
    if(cmd_msg.data==2){
-        digitalWrite(13,HIGH);
-        delay(1000);
-        digitalWrite(13,LOW);
         servo2.write(50);
    }
    if(cmd_msg.data==3){
-        digitalWrite(13,HIGH);
-        delay(2000);
-        digitalWrite(13,LOW);
         servo2.write(180);
         delay(1000);
         servo2.write(0);
    }
 }
 
+void verifierContacteur(){
+  if(contact == 0){
+    if(analogRead(A15)>600){
+      for(int i=0;i<=5;i++){
+        if(analogRead(A15)<600){
+          i=6;
+        }
+        if(i==5){
+           contact = 1;
+           ordreDepart.data = true;
+           depart.publish(&ordreDepart);
+        }
+      }
+    }
+  }
+  if(contact == 1){
+    if(analogRead(A15)<100){
+      contact = 0;
+      ordreDepart.data = false;
+      depart.publish(&ordreDepart);
+    }
+  }
+}
+
 void setup(){
   
   //Diode
-  pinMode(13, OUTPUT);
+  pinMode(13,OUTPUT);
   digitalWrite(13,LOW);
   
   //Contacteur
@@ -66,51 +78,24 @@ void setup(){
   Serial.begin(57600);
 
   servo1.attach(7);
-  servo1.write(25);
-
+  servo1.write(0);
+  servo1.write(0);
+  
   servo2.attach(9);
   servo2.write(0);
 
   nc.subscribe(sub);
-
 }
 
 void loop()
 {
-  ordreDepart.data = false;
-  while(contact == 0){
-    depart.publish(&ordreDepart);
-    delay(500);
-    nc.spinOnce();
-    if(analogRead(A15)>600){
-      for(int i=0;i<=5;i++){
-        digitalWrite(13,HIGH);
-        delay(50);
-        digitalWrite(13,LOW);
-        delay(100);
-        if(analogRead(A15)<600){
-          i=6;
-        }
-        if(i==5){
-           contact = 1;
-           digitalWrite(13,HIGH);
-           ordreDepart.data = true;
-           depart.publish(&ordreDepart);
-           nc.spinOnce();
-           delay(1000);
-           digitalWrite(13,LOW);
-         }
-      }
-    }
-  }
-  while(contact == 1){
-    nc.spinOnce();
-    delay(1);
-    if(analogRead(A15)<100){
-      contact = 0;
-      digitalWrite(13,HIGH);
-      delay(200);
-      digitalWrite(13,LOW);
-    }
-  }
+  digitalWrite(13,HIGH);
+  delay(1000);
+//  verifierContacteur();
+//  nc.spinOnce();
+  delay(1000);
+  servo1.write(90);
+  digitalWrite(13,LOW);
+  delay(1000);
+  servo1.write(0);
 }
